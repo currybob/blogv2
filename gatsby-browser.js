94246765023d4f729e7de9ee6bdf180126791a90
Wonho Kim <rhwhdeodhkd@naver.com>
2019-05-08 10:24:33 +0900

    modify service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,19 +1,24 @@
-export const onServiceWorkerUpdateFound = () => {
+export const onServiceWorkerUpdateReady = () => {
   const answer = window.confirm(
     '버전이 업데이트되었습니다.\n최신 버전으로 새로고침하시겠습니까?'
   )
 
-  if (answer === true) {
-    window.location.reload()
+  if (answer) window.location.reload()
+}
+
+export const onServiceWorkerRedundant = ({ serviceWorker }) => {
+  if (typeof window !== 'undefined' && 'serviceWorker' in window.navigator) {
+    serviceWorker.getRegistrations().then(registrations => {
+      registrations.forEach(r => r.unregister())
+    })
   }
 }
 
 export const onServiceWorkerActive = ({ serviceWorker }) => {
-  console.log(serviceWorker)
   serviceWorker.registration.showNotification('안녕하세요', {
     body: '김원호 블로그에 오신 것을 환영합니다',
     icon: 'src/assets/images/photo.jpg',
   })
 }
 
-export const registerServiceWorker = () => true
+// export const registerServiceWorker = () => true
